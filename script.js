85fdc782b001b7d49fbc6551292f7589242b991d
Robert Johnsen <42631146+RoberCop@users.noreply.github.com>
2019-05-25 22:42:03 +0200

    added remove buttons, refactoring and comments

--- script.js
+++ script.js
@@ -20,23 +20,21 @@ function treeNodeClass(level, parent, index, text)
 {
 	this.childs = [];
 
-	this.level = level;
-	this.parent = parent;
-	
 	this.selectedChild = 0;
-
-	let indexOfThis = index;
 
 	// adds a new instance to "childs" array
 	this.newChild = function(newText)
 	{
-		this.childs.push(new treeNodeClass(this.level + 1, this, this.childs.length, newText));
+		this.childs.push(new treeNodeClass(level + 1, this, this.childs.length, newText));
 	}
 
 	this.addToDisplay = function(isSelected)
 	{
-		displayArray[this.level - 1].push(this);
+		// add this node to the displayArray if its not root
+		if (level > 0)
+			displayArray[level - 1].push(this);
 
+		// if selected, call this same method on child objects
 		if (isSelected)
 			for (childIndex in this.childs)
 				this.childs[childIndex].addToDisplay(childIndex == this.selectedChild);
@@ -52,35 +50,59 @@ function treeNodeClass(level, parent, index, text)
 	this.childPar = document.createElement("p");
 	this.childPar.innerText = text;
 
+	this.removeBtn = document.createElement("button");
+	this.removeBtn.innerText = "X";
+	this.removeBtn.style.right = 0;
+	this.removeBtn.style.top = 0;
+	this.removeBtn.style.visibility = "hidden";
+
 	this.domElem.appendChild(this.childPar);
+	this.domElem.appendChild(this.removeBtn);
 
 	this.domElem.onclick = function()
 	{
-		for (let i = this.treeNode.level - 1; i < displayArray.length; i++)
+		// clear this stack and outwards in the display array
+		for (let i = level - 1; i < displayArray.length; i++)
 			displayArray[i] = [];
 
-		this.treeNode.parent.selectedChild = indexOfThis;
-		this.treeNode.parent.addToDisplay(true);
-		drawNodes(this.treeNode.level - 1);
+		// hides last selected node's "removeBtn", and makes this node's button visible
+		parent.childs[parent.selectedChild].removeBtn.style.visibility = "hidden"; 
+		this.treeNode.removeBtn.style.visibility = "visible";
+
+		// set this node to be the selected child, on the parent
+		parent.selectedChild = index;
+
+		// recursively add to display based on selected nodes from parent node, and draw the new nodes
+		parent.addToDisplay(true);
+		drawNodes(level - 1);
 	}
 
-	this.domElem.draw = function(nodeIndex)
+	this.domElem.setStyle = function(nodeIndex)
 	{
-		this.style.left = (101 * nodeIndex);
-		this.style.backgroundColor = (this.treeNode.parent.selectedChild == indexOfThis) ? "#CCCCFF" : "#FFFFFF";
+		this.style.left = (151 * nodeIndex);
+		this.style.backgroundColor = (parent.selectedChild == index) ? "#CCCCFF" : "#FFFFFF";
+	}
+
+	this.removeBtn.onclick = function()
+	{
+		parent.selectedChild = 0;
+		parent.childs.splice(index, 1);
+		index = 0;
 	}
 }
 
-// Example for drawing nodes
+// Redraw all nodes from a stack and outward
 function drawNodes(level)
 {
 	for (let i = level; i < displayArray.length; i++)
 	{
+		// clear dom elements in the stack
 		treeDiv.children[i].innerHTML = "";
 
+		// add each node to the stack
 		for (let j = 0; j < displayArray[i].length; j++)
 		{
-			displayArray[i][j].domElem.draw(j);
+			displayArray[i][j].domElem.setStyle(j);
 			treeDiv.children[i].appendChild(displayArray[i][j].domElem);
 		}
 	}
@@ -89,6 +111,7 @@ function drawNodes(level)
 // draw all the nodes from root the first time
 function firstDraw()
 {
+	// fill in the displayArray from root
 	for (childIndex in root.childs)
 		root.childs[childIndex].addToDisplay(childIndex == root.selectedChild);
 
@@ -96,8 +119,9 @@ function firstDraw()
 }
 
 // Makes the root node, meant to be invisible to the user
-const root = new treeNodeClass(0, null, null, null);
+const root = new treeNodeClass(0, this, 0, null);
 
+// becomes two-dimentional array, holds objects meant to be displayed
 const displayArray = [];
 
 // Makes 10 stacks to start with
