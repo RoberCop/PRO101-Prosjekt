17a4a193bd3d08a2ffd9df12a3e40a039a43f9c5
Hans Maast <hfmaast@gmail.com>
2019-05-27 12:31:55 +0200

    minor style adjustments

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ const root = new treeNodeClass(0, this, 0, null);
 const displayArray = [];
 
 // Makes 10 stacks to start with
-for (let i = 0; i < 10; i++)
+for (let i = 0; i < 30; i++)
 	newTreeStack();
 
 // Hard code sample nodes for testing
@@ -144,14 +144,19 @@ root.childs[0].newChild("Make a house");
 root.childs[0].newChild("Make a car");
 root.childs[0].newChild("Make a computer");
 
+//House
 root.childs[0].childs[0].newChild("Make a foundation");
 root.childs[0].childs[0].newChild("Make walls");
 root.childs[0].childs[0].newChild("Make a roof");
 root.childs[0].childs[0].newChild("Make windows");
 root.childs[0].childs[0].newChild("Make a door");
+root.childs[0].childs[0].newChild("test");
+root.childs[0].childs[0].newChild("test");
 
+root.childs[0].childs[0].childs[0].newChild("Get some concrete");
 root.childs[0].childs[0].childs[1].newChild("Get some wood");
 
+//Car
 root.childs[0].childs[1].newChild("Make a engine");
 root.childs[0].childs[1].newChild("Make a frame");
 root.childs[0].childs[1].newChild("Make some wheels");
