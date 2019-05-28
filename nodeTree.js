8d1c22af389c750246e7097cfed562db3b20e2f1
Robert Johnsen <42631146+RoberCop@users.noreply.github.com>
2019-05-28 16:35:30 +0200

    refactor of deletePrompt

--- nodeTree.js
+++ nodeTree.js
@@ -1,3 +1,12 @@
+// Dom element constants
+const treeDiv = document.getElementById("treeDiv"); 
+const html = document.querySelector('html');
+const body = document.querySelector('body');
+const shader = document.createElement('div');
+shader.setAttribute('class', 'shader');
+
+const deleteBtn = document.getElementById("deleteNode"); 
+
 function addNodeBtnClass(level, parent, index)
 {
 	this.indexOfThis = index;
@@ -32,8 +41,55 @@ function addNodeBtnClass(level, parent, index)
 	}
 }
 
-// Dom element constants
-const treeDiv = document.getElementById("treeDiv"); 
+function createWarningElem()
+{
+	//Module
+	const module = document.createElement('div');
+	module.dataset.clicked = "true";
+
+	//Title
+	const h1 = document.createElement('h1');
+	h1.innerText = 'Hold it!';
+
+	//Paragraph in module
+	const paragraph = document.createElement('p');
+	paragraph.innerText = 'This wil also delete the child nodes. Are you sure you want to delete this?';
+
+	//Button container
+	const btnContainer = document.createElement('div');
+	btnContainer.setAttribute('class', 'edit-btn-container');
+
+	//Delete button
+	const deleteBtn = document.createElement('div');
+	deleteBtn.innerText = "Delete anyway";
+	btnContainer.appendChild(deleteBtn);
+
+	//Cancel button
+	const cancelBtn = document.createElement('h4');
+	cancelBtn.dataset.btn = "cancel";
+	cancelBtn.innerText = "Cancel";
+	btnContainer.appendChild(cancelBtn);
+
+	cancelBtn.onclick = function()
+	{
+		body.removeChild(shader);
+	}
+	
+	//Attributes
+	module.setAttribute('class', 'module');
+	deleteBtn.setAttribute('class', 'red deleteBtn module-btn');
+	cancelBtn.setAttribute('class', 'green cancelBtn module-btn');
+
+	module.appendChild(h1);
+	module.appendChild(paragraph);
+	module.appendChild(btnContainer);
+	shader.appendChild(module);
+}
+
+deleteNode.onclick = function()
+{
+	body.appendChild(shader);
+}
 
 /* Makes stacks inside "treeDiv", causing displayArray to become
  * a two-dimentional array
@@ -81,69 +137,7 @@ for (let i = 0; i < 30; i++)
 
 const root = getTreeData();
 root.newAddBtnRec();
+
+createWarningElem();
+
 firstDraw();
-
-//Creates a warning when deleting node
-function renderWarning(){
-	//Gets body element
-	const body = document.querySelector('body');
-	//Blurs background
-	const shader = document.createElement('div');
-	shader.setAttribute('class', 'shader');
-	//Module
-	const module = document.createElement('div');
-	module.dataset.clicked = "true";
-	const h1 = document.createElement('h1');
-	h1.innerText = 'Hold it!';
-	//Paragraph in module
-	const paragraph = document.createElement('p');
-	paragraph.innerText = 'This wil also delete the child nodes. Are you sure you want to delete this?';
-	//Button container
-	const btnContainer = document.createElement('div');
-	btnContainer.setAttribute('class', 'edit-btn-container');
-	//Delete button
-	const deleteBtn = document.createElement('div');
-	deleteBtn.innerText = "Delete anyway";
-	btnContainer.appendChild(deleteBtn);
-	//Cancel button
-	const cancelBtn = document.createElement('h4');
-	cancelBtn.dataset.btn = "cancel";
-	cancelBtn.innerText = "Cancel";
-	btnContainer.appendChild(cancelBtn);
-	//Attributes
-	module.setAttribute('class', 'module');
-	deleteBtn.setAttribute('class', 'red deleteBtn module-btn');
-	cancelBtn.setAttribute('class', 'green cancelBtn module-btn');
-	//Appends to child
-	body.appendChild(shader);
-	shader.appendChild(module);
-	module.appendChild(h1);
-	module.appendChild(paragraph);
-	module.appendChild(btnContainer);
-}
-
-//Removing waring for deleting
-function removeWaring(){
-	//Gets body element
-	const body = document.querySelector('body');
-	//Gets shader
-	const shader = document.querySelector('.shader');
-	//Removes shader
-	shader.removeChild(shader.firstChild);
-	body.removeChild(shader);
-}
-
-//Eventlisteners
-const html = document.querySelector('html');
-const body = document.querySelector('body');
-document.addEventListener('click', e => {
-	if (e.target.dataset.btn == "delete" && e.target.dataset.clicked != 'true')
-	{
-		renderWarning();
-	}
-	if (e.target.dataset.btn == 'cancel'){
-		removeWaring();
-	}
-})
-
-
