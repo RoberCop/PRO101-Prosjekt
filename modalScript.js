94dd745662540696f94abbd83ac3e49918e2aac9
Petlas88 <42611458+Petlas88@users.noreply.github.com>
2019-05-31 14:48:31 +0200

    Added styling, some functions

--- modalScript.js
+++ modalScript.js
@@ -4,19 +4,26 @@
 var signupModal = document.getElementById('signup-modal');
 var signedUp = document.getElementById('signed-up-modal')
 var signUpForm = document.getElementById('signup-form')
+var easterEgg = document.getElementById("reasons-modal");
+
 //Get open modal button
 var modalBtn = document.getElementById('signup-btn');
+var easterEggBtn = document.getElementById('why-btn');
 
 // Get close button
 var closeBtn = document.getElementById('close-modal');
 var closeBtn2 = document.getElementById('close-modal2');
+var closeBtn3 = document.getElementById('close-modal3')
 
 //Listen for open click
 modalBtn.addEventListener('click', openModal);
+easterEggBtn.addEventListener('click', openModal3);
 
 //Listen for close click
 closeBtn.addEventListener('click', closeModal);
 closeBtn2.addEventListener('click', closeModal2);
+closeBtn3.addEventListener('click', closeModal3);
+
 //Close window click
 window.addEventListener('click', outsideClick);
 
@@ -25,23 +32,30 @@ function openModal() {
     signupModal.style.display = 'flex';
 }
 
+function openModal3() {
+    easterEgg.style.display = 'flex';
+}
 //Function to close modal
 function closeModal() {
     signupModal.style.display = 'none';
     signUpForm.reset();
     removeMessage();
-
 }
+
 function closeModal2() {
     signedUp.style.display = 'none';
+}
 
+function closeModal3() {
+    easterEgg.style.display = 'none';
 }
 
 //Function for window click close
 function outsideClick(e) {
-    if (e.target == signupModal || e.target == signedUp) {
+    if (e.target == signupModal || e.target == signedUp || e.target == easterEgg) {
         signupModal.style.display = 'none';
         signedUp.style.display = 'none';
+        easterEgg.style.display = 'none';
         signUpForm.reset();
         removeMessage();
     }
