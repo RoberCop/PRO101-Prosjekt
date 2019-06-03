8810e560abb0fb650e3c04573a4f4f51afe1e9a6
Petlas88 <42611458+Petlas88@users.noreply.github.com>
2019-06-03 12:08:11 +0200

    added usability features, changed some styling

--- chat.js
+++ chat.js
@@ -9,6 +9,7 @@ openChat.addEventListener('click', openChatWindow);
 closeChatBtn.addEventListener('click', closeChat);
 sendMsgBtn.addEventListener('click', sendMessage);
 msgInput.addEventListener('keyup', enterSend);
+chatWindow.addEventListener('keyup', escCloseChat);
 
 function openChatWindow()
 {
@@ -22,14 +23,16 @@ function closeChat()
 }
 
 function sendMessage() 
-{
+{ 
+    if(msgInput.value.length > 0) 
+    {
     var contentDiv = document.createElement('div');
     contentDiv.className = 'chat-msg'; 
     contentDiv.innerHTML = 'You: ' + msgInput.value; 
     chatBody.appendChild(contentDiv);
     msgInput.value ="";
     msgInput.focus();
-    
+    }  
 }
 
 function enterSend(event) 
@@ -38,5 +41,12 @@ function enterSend(event)
         sendMessage();
     }
 }
+
+function escCloseChat(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        closeChat();
+    }
+};
    
 
