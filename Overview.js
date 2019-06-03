ff66e59bbdcaece62ad22725228534fa5dd0c9c0
unknown <lassehovden@gmail.com>
2019-06-04 00:28:46 +0200

    new styling to overwiev and js rewritten

--- Overview.js
+++ Overview.js
@@ -23,8 +23,9 @@ var completePro=[];
 
 function showform() {
 
-   form.style.display = "block";
-    
+form.style.display = "block";
+form.style.animation = 'growing 2s forwards';
+   
     
    
 }
@@ -32,57 +33,35 @@ function showform() {
 
 function closeform() {
 form.style.display = "none";
+
 }   
 
 
 
 function createProject() {
-
+  var warning=document.getElementById("warning1");
   var namepro= document.getElementById("Projectname").value;
+if(namepro == ""){
+    
+   warning.innerHTML="You must write something!";
+    
+    createProject();
+}
 
- var type= document.getElementById("type").value;
     
 var divpro = document.createElement("div");
 var feedpro = document.createElement("div");
   
 divpro.innerHTML=namepro;
 divpro.className="projectdiv";
-    divpro.setAttribute("id",namepro);
-    divpro.setAttribute("draggable",true);
-   divpro.setAttribute("ondragstart","drag(event)","ondragend(dragend())");
-    
-   
-    
-
-    
+divpro.setAttribute("id",namepro);
+divpro.setAttribute("draggable",true);
+divpro.setAttribute("ondragstart","drag(event)","ondragend(dragend())");
 
 feedpro.className="feeddiv";
 
 
 
-
-
-if(type == "inprogress"){
-
-  var inps= " In Progress";  
-feedpro.innerHTML="User Crated Project"+"<br />"+ " Called: "+namepro +"<br />"+" In the: "+ inps+ " section.";
-
- document.getElementById("Feedup1").appendChild(feedpro);   
-document.getElementById("IpProject1").appendChild(divpro);
- 
-    namepro={name:namepro,Type:inps,}
-    inProgressPro.push(namepro);
-    
-    closeform();
-    
-  
-
-}
-    
-    
-    
-if(type == "upcoming"){
-    
 var upcs= " Upcoming";  
 feedpro.innerHTML="User Crated Project"+"<br />"+ " Called: "+namepro +"<br />"+" In the: "+ upcs+ " section.";
     
@@ -91,61 +70,63 @@ document.getElementById("Feedup1").appendChild(feedpro);
     
 document.getElementById("UpProject1").appendChild(divpro);
  
-       namepro={name:namepro,Type:upcs,}
+    namepro={name:namepro}
     upcomPro.push(namepro);
     
     
     closeform();   
-}   
+   
     
-    
- 
-  
-    
-    
-console.table(inProgressPro);
-    console.table(upcomPro);
-
-
 }
 
 
 /* drag and drop funtion START with const */
 
 
-const proheadcomp= document.getElementById("CompletedHeader");
-const proheadimp= document.getElementById("InprogressHeader");
-const proheadcupc= document.getElementById("UpcomingHeader");
+
 const deleteElement=document.getElementById("Deleteprojectcont");
 
-function opentabdragComp(){
 
-allowDrop(event);
-showcomp();
-proheadcomp.style.transform="scale(1.2)"; 
-
-}
-
-
-function opentabdragImp(){
-proheadimp.style.transform="scale(1.2)"; 
-allowDrop(event);
-showimp(); 
-    
+function dragleaveComp(){
+   compCon.style.transform="scale(1.0)"; 
    
 }
 
 
-function opentabdragcUpc(){
-proheadcupc.style.transform="scale(1.2)"; 
+function dragleaveImp(){
+    impCon.style.transform="scale(1.0)"; 
     
-allowDrop(event);
-showupc();    
+}
+
+
+function dragleaveUpc(){
+    upcCon.style.transform="scale(1.0)"; 
+    
+}
+
+function dragoverComp(ev){
+   allowDrop(event);
+    compCon.style.transform="scale(1.05)"; 
+
+}
+
+function dragoverImp(ev){
+   allowDrop(event);
+    impCon.style.transform="scale(1.05)";  
+
+}
+function dragoverUpc(ev){
+ allowDrop(event);
+
+    upcCon.style.transform="scale(1.05)"; 
 }
 
 
 function allowDrop(ev) {
-    
+   
+    if(event.target.className == "projectdiv"){
+        return;
+    }
      
     ev.preventDefault(); 
 }
@@ -167,37 +148,94 @@ function drop(ev) {
   var data = ev.dataTransfer.getData("text");
    ev.target.appendChild(document.getElementById(data));
 
-}
-
-
-function dragleaveComp(){
-    proheadcomp.style.transform="scale(1.0)"; 
-   
-}
-
-
-function dragleaveImp(){
-    proheadimp.style.transform="scale(1.0)"; 
+    if(ev.target.id == "CompProject1"){
+    data={name:data};
+    completePro.push(data);
+var feedpro = document.createElement("div");
+feedpro.className="feeddiv";
+        
+document.getElementById("Feedup1").appendChild(feedpro);   
     
-}
-
-
-function dragleaveUpc(){
-    proheadcupc.style.transform="scale(1.0)"; 
+feedpro.innerHTML="User moved Project:"+data+ " to: Complete section";
     
+        for (var i=0; i < upcomPro.length; i++) {
+        if (upcomPro[i].name === data) {
+             upcomPro[i].splice(i, 1); ;
+        }
+     }
+        
+        for (var i=0; i < inProgressPro.length; i++) {
+        if (inProgressPro[i].name === data) {
+             inProgressPro[i].splice(i, 1); ;
+        }
+     }
+  }
+    
+      if(ev.target.id == "UpProject1"){
+    data={name:data};
+    upcomPro.push(data);
+          
+    var feedpro = document.createElement("div");
+feedpro.className="feeddiv";
+        
+document.getElementById("Feedup1").appendChild(feedpro);   
+    
+feedpro.innerHTML="User moved Project"+data+ " to: Upcoming section";
+    
+        for (var i=0; i < completePro.length; i++) {
+        if (completePro[i].name === data) {
+             completePro[i].splice(i, 1); ;
+        }
+     }
+        
+        for (var i=0; i < inProgressPro.length; i++) {
+        if (inProgressPro[i].name === data) {
+             inProgressPro[i].splice(i, 1); ;
+        }
+     }
+  }
+    
+    
+    
+    
+    if(ev.target.id == "IpProject1"){
+    data={name:data};
+    inProgressPro.push(data);
+
+var feedpro = document.createElement("div");
+feedpro.className="feeddiv";
+        
+document.getElementById("Feedup1").appendChild(feedpro);   
+    
+feedpro.innerHTML="User moved Project"+data+ " to: Inprogress section";
+    
+        for (var i=0; i < completePro.length; i++) {
+        if (completePro[i].name === data) {
+             completePro[i].splice(i, 1); ;
+        }
+     }
+        
+        for (var i=0; i < upcomPro.length; i++) {
+        if (upcomPro[i].name === data) {
+             upcomPro[i].splice(i, 1); ;
+        }
+     }
+  }
+    
+  console.table(inProgressPro);
+    console.table(upcomPro);  
+    console.table(completePro); 
 }
 
 
-function dragleaveDel(){
-  
- 
-}
+
+
 
 
 function dragoverdel(){
    
 allowDrop(event);
-    
+ deldrop(ev);   
     
 }
 
@@ -211,13 +249,13 @@ ev.preventDefault();
 var data=ev.dataTransfer.getData("Text");
 var el = document.getElementById(data);
 el.parentNode.removeChild(el);
+
    
 document.getElementById("Feedup1").appendChild(feedpro);   
     
 feedpro.innerHTML="User DELETED Project"+"<br />"+ " Called: "+data;
     
 
-    
 }
 
 
